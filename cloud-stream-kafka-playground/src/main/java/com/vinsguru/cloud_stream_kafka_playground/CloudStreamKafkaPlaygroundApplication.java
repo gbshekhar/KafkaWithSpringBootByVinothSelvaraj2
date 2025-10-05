0bd540570cb561cb979bf18c8487211288ab9d12
Bheemshekhar <bheemshekhar.golla@gmail.com>
2025-10-05 17:38:04 +0530

    Spring

---
+++ cloud-stream-kafka-playground/src/main/java/com/vinsguru/cloud_stream_kafka_playground/CloudStreamKafkaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.vinsguru.cloud_stream_kafka_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamKafkaPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStreamKafkaPlaygroundApplication.class, args);
+	}
+
+}
---
+++ cloud-stream-kafka-playground/src/test/java/com/vinsguru/cloud_stream_kafka_playground/CloudStreamKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinsguru.cloud_stream_kafka_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamKafkaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
